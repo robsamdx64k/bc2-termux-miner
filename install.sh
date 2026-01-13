61c232a8bf30108ad605547a18d892d04cf85808
robsamdx64k <134449912+robsamdx64k@users.noreply.github.com>
2026-01-13 18:59:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -e
 
-echo "🔥 bc2-termux-miner installer"
+echo "🔥 BC2 Miner armv8 Installer"
 echo "By BobFarms / DeskNuts / ChatGPT"
 echo "================================"
 

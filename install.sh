1b34aeb56d8d7ed6acb6f35bcb996dc3915a0c05
robsamdx64k <134449912+robsamdx64k@users.noreply.github.com>
2026-01-13 19:01:43 -0500

    Enhance installer message with additional emojis

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -e
 
-echo "🔥 BC2 Miner armv8 Installer"
+echo "🔥💰🖨️ BC2 Miner armv8 Installer 🔥🔥🔥"
 echo "By BobFarms / DeskNuts / ChatGPT"
 echo "================================"
 
